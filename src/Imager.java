8378ab5ef3847bf4ec970acefea11752a50df848
Emmett <Emmett@Emmett-PC>
2016-09-27 22:06:06 -0700

    HIYA HELLOA

---
+++ src/Imager.java
@@ -0,0 +1,69 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Imager {
+	
+	public Window window;
+	
+	public int[][] grid = {
+			{0,0,7,7,0,0,0,0,0,0},
+			{0,0,0,0,0,0,2,0,0,0},
+			{1,0,0,0,1,0,7,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,1,0,1,0,0},
+			{0,3,0,0,0,0,1,0,0,0},
+			{0,0,0,1,0,0,0,0,0,0},
+			{0,0,0,0,1,0,0,2,0,0},
+			{0,1,2,3,4,5,6,7,8,0},
+			{0,0,0,5,0,0,0,0,0,0}
+	};
+	
+	public Imager() throws IOException{
+		window = new Window();
+		while(true){
+			window.canvas.repaint();
+		}
+	}
+	
+	public static void rect(BufferedImage b, Color c, int x, int y, int w, int h){
+		Graphics2D g = b.createGraphics();
+		g.setColor(c);
+		g.fillRect(x, y, w, h);
+	}
+	
+	public static void arrayToRect(BufferedImage b, int[][] a){
+		Color[] colors = {Color.WHITE,Color.GRAY,Color.RED,Color.ORANGE,Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA};
+		for(int y=0;y<a.length;y++){
+			for(int x=0;x<a[y].length;x++){
+				try{
+				rect(b,colors[a[y][x]],x,y,1,1);
+				}catch(Exception e){}
+			}
+		}
+	}
+	
+	public static BufferedImage makeImage(int x,int y){
+		BufferedImage img = new BufferedImage(x,y, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = img.createGraphics();
+		g.setColor(new Color(0,0,0,0));
+		g.fillRect(0, 0, x, y);
+		return img;
+	}
+	
+	public static void SaveImage(BufferedImage b, String name){
+		File f = new File(name);
+		try {
+			ImageIO.write(b, "PNG", f);
+		}catch (IOException e){}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new Imager();
+	}
+
+}
---
+++ src/Window.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.MouseInfo;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame{
+	
+	Canvas canvas;
+	JButton j;
+	JTextField inp;
+	int[][] block = new int[16][16];
+	
+	public class Canvas extends JPanel{
+		public void paintComponent(Graphics g){
+			Color[] colors = {Color.WHITE,Color.GRAY,Color.RED,Color.ORANGE,Color.YELLOW,Color.GREEN,Color.BLUE,Color.MAGENTA};
+			super.paintComponent(g);
+			setBackground(Color.GRAY);
+			for(int y=0;y<block.length;y++){
+				for(int x=0;x<block.length;x++){
+					g.setColor(colors[block[y][x]]);
+					g.fill3DRect(x*32+(getWidth()/2)-(block.length*16), y*32+(getHeight()/2)-(block.length*16), 32, 32, true);
+				}
+			}
+		}
+	}
+	
+	public Window(){
+		setTitle("Image Maker");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 640);
+		canvas = new Canvas();
+		canvas.addMouseListener(new MListen());
+		j = new JButton("Create");
+		inp = new JTextField();
+		j.addActionListener(new CListen());
+		JPanel bc = new JPanel();
+		bc.setLayout(new GridLayout());
+		bc.add(j);
+		bc.add(inp);
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+	    cp.add(canvas,BorderLayout.CENTER);
+	    cp.add(bc, BorderLayout.SOUTH);
+	    //setResizable(false);
+		setVisible(true);
+	}
+	
+	public class CListen implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			BufferedImage img = Imager.makeImage(block[0].length,block.length);
+			Imager.arrayToRect(img,block);
+			Imager.SaveImage(img,inp.getText()+".png");
+		}
+	}
+	
+	public class MListen implements MouseListener{
+	    
+		public void mouseClicked(MouseEvent e) {
+			try{
+				block[(block.length*16)/32-(getHeight()/2)/32+e.getY()/32+1][(block.length*16)/32-(getWidth()/2)/32+e.getX()/32]++;
+			}catch(Exception ex){
+			}
+		}
+		public void mouseEntered(MouseEvent arg0) {
+		}
+		public void mouseExited(MouseEvent arg0) {
+		}
+		public void mousePressed(MouseEvent arg0) {
+		}
+		public void mouseReleased(MouseEvent arg0) {
+		}
+
+	}
+	
+}
